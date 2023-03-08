6d04556fcd82a4d460032cbf60c2a16dba200834
Samuel Swee <samsjq@gmail.com>
2023-03-08 22:08:00 +0800

    Tweet button

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ document.getElementById('submitbtm').addEventListener('click', function() {
     var ctx = canvas.getContext("2d");
     var imageObj = new Image();
     document.getElementById('sample').style.display = 'none';
+    document.getElementById('tweet').style.opacity = '1';
     document.getElementById('myCanvas').style.display = 'block';
     text_name = document.getElementById('name').value;
     imageObj.onload = function(){
         ctx.drawImage(imageObj, 0, 0);
-        ctx.font = "36pt alataregular";
+        ctx.font = "40pt alataregular";
         ctx.fillStyle = '#004aad';
         ctx.textBaseline = 'middle';
         ctx.textAlign = "center";
@@ -15,3 +16,20 @@ document.getElementById('submitbtm').addEventListener('click', function() {
     };
     imageObj.src = "nft_square.jpg"; 
 });
+
+window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+  
+    return t;
+  }(document, "script", "twitter-wjs"));
